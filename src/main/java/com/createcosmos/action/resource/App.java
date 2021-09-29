eceb15af4a210f31d4eef5cd2cd18bc18777efbe
mahines <marcus.hines@microsoft.com>
2021-09-29 12:51:33 -0400

    able to connect to db. Permissions dont allow us to create, delted db or containers, etc.

--- src/main/java/com/createcosmos/action/resource/App.java
+++ src/main/java/com/createcosmos/action/resource/App.java
@@ -1,62 +1,76 @@
 package com.createcosmos.action.resource;
 
-import com.azure.cosmos.CosmosClient;
-import com.azure.cosmos.CosmosClientBuilder;
+import com.azure.cosmos.*;
+import com.azure.cosmos.models.CosmosDatabaseResponse;
+import com.azure.cosmos.models.CosmosItemResponse;
+import com.azure.cosmos.models.PartitionKey;
 import com.azure.identity.*;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.microsoft.azure.documentdb.Database;
+import com.microsoft.azure.documentdb.Document;
+import org.springframework.beans.factory.config.CustomEditorConfigurer;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class App {
-    private final String databaseName = "ToDoList";
-    private final String containerName = "Items";
-    private final String endpoint = "https://mahines-cosmosdb.documents.azure.com:443/";
+    private final String DATABASE_NAME = "DemoDatabase";
+    private final String CONTAINER_NAME = "CustomerDetails";
+    private final String ENDPOINT = "https://mahinescosmos.documents.azure.com:443/";
+    public final String MANAGED_IDENTITY_CLIENT_ID = "5bca3991-8179-4edc-86b4-fb8a145d6f6d";
 
-    public final String MANAGED_IDENTITY_CLIENT_ID = "";
-    public final String VAULT_URL = "";
-
+    private CosmosContainer container;
+    private CosmosDatabase database;
+    private CosmosClient cosmosClient;
 
     public App(String databaseName) {
-        //this.databaseName = databaseName;
+        //Managed Identity Credentials
+        ManagedIdentityCredential msi = createMSICredentials();
+
+        this.cosmosClient = createCosmosClient(msi);
+        this.database = cosmosClient.getDatabase(DATABASE_NAME);
+        this.container = database.getContainer(CONTAINER_NAME);
     }
 
     public void run() {
         System.out.println("App is starting...");
-        //Managed Identity Credentials
-        DefaultAzureCredential azureCredential = createDefaultAzureCredentialForUserAssignedManagedIdentity();
-
-        //Cosmos DB client
-        //CosmosClient cosmosClient = createCosmosClient(azureCredential);
-
-       // CosmosDatabaseResponse response = cosmosClient.createDatabaseIfNotExists(this.databaseName);
-
-
-       // System.out.println(response.getStatusCode());
+        this.getCustomer("24");
+        this.cosmosClient.close();
     }
 
-    private CosmosClient createCosmosClient(DefaultAzureCredential azureCredential){
-        return new CosmosClientBuilder().credential(azureCredential).buildClient();
+    private Customer getCustomer(String customerID){
+        CosmosItemResponse<Customer> response = this.container.readItem(customerID, new PartitionKey("Sweets"), Customer.class);
+        Customer customer = response.getItem();
+        System.out.println("Returning customer: " + customer.getFirstname() + " " + customer.getLastname());
+
+        return customer;
     }
 
-    /**
-     * The default credential will use the user assigned managed identity with the specified client ID.
-     */
-    public DefaultAzureCredential createDefaultAzureCredentialForUserAssignedManagedIdentity() {
-        return new DefaultAzureCredentialBuilder()
-                .managedIdentityClientId(MANAGED_IDENTITY_CLIENT_ID)
-                .build();
+    private CosmosClient createCosmosClient(ManagedIdentityCredential azureCredential){
+        if(azureCredential == null){
+            System.out.println("The given azureCredential object is null in createCosmosClient");
+        }
+        return new CosmosClientBuilder().credential(azureCredential).endpoint(ENDPOINT).buildClient();
     }
 
-    private SecretClient createClientWithCredentials(DefaultAzureCredential defaultCredential){
-        // Azure SDK client builders accept the credential as a parameter
-        return new SecretClientBuilder()
-                .vaultUrl(VAULT_URL)
-                .credential(defaultCredential)
-                .buildClient();
+    private ManagedIdentityCredential createMSICredentials(){
+        return new ManagedIdentityCredentialBuilder().clientId(MANAGED_IDENTITY_CLIENT_ID).build();
     }
+
+/*
+    // Database Create
+    private void createDatabaseIfNotExists() throws Exception {
+        //logger.info("Create database " + databaseName + " if not exists...");
+
+        //  Create database if not exists
+        CosmosDatabaseResponse databaseResponse = client.createDatabaseIfNotExists(databaseName);
+        database = client.getDatabase(databaseResponse.getProperties().getId());
+
+        logger.info("Done.");
+    }
+
 
     private List<Customer> generateApplicationData(){
         List<Customer> applicationData =  new ArrayList<Customer>();
@@ -71,13 +85,29 @@ public class App {
 
         return applicationData;
     }
-}
-/*
-2021-09-20 13:06:04.175 ERROR 1 --- [
-   main] c.azure.identity.EnvironmentCredential:
-    Azure Identity => ERROR in EnvironmentCredential: Failed to determine an authentication scheme based on the available environment variables.
-    Please specify AZURE_TENANT_ID and AZURE_CLIENT_SECRET to authenticate through a ClientSecretCredential;
-    AZURE_TENANT_ID and AZURE_CLIENT_CERTIFICATE_PATH to authenticate through a ClientCertificateCredential;
-    or AZURE_USERNAME and AZURE_PASSWORD to authenticate through a UserPasswordCredential.
 
- */
+
+
+    private void createDocument() throws Exception {
+        logger.info("Create document " + documentId);
+
+        // Define a document as a POJO (internally this
+        // is converted to JSON via custom serialization)
+        Family family = new Family();
+        family.setLastName(documentLastName);
+        family.setId(documentId);
+
+        // Insert this item as a document
+        // Explicitly specifying the /pk value improves performance.
+        container.createItem(family,new PartitionKey(family.getLastName()),new CosmosItemRequestOptions());
+
+        logger.info("Done.");
+    }
+
+    private void queryAllDocuments() throws Exception {
+        logger.info("Query all documents.");
+
+        executeQueryPrintSingleResult("SELECT * FROM c");
+    }
+*/
+}
